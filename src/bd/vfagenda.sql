073cca643adee2798b2ef831d3e58ccd796b5fb7
Ronaldo <Ronaldo@Ronaldo-PC>
2017-02-26 22:42:19 -0400

    altas coisas com bd

--- src/bd/vfagenda.sql
+++ src/bd/vfagenda.sql
@@ -2,10 +2,10 @@
 -- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 20-Fev-2017 às 22:48
+-- Host: localhost
+-- Generation Time: 27-Fev-2017 às 03:41
 -- Versão do servidor: 10.1.13-MariaDB
--- PHP Version: 7.0.6
+-- PHP Version: 5.5.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `cad_contato` (
   `id_contato` int(11) NOT NULL,
   `nome` text NOT NULL,
-  `data_nasc` date NOT NULL,
+  `data_nasc` text NOT NULL,
   `endereco` text NOT NULL,
   `bairro` text NOT NULL,
   `cep` text NOT NULL,
@@ -41,16 +41,42 @@ CREATE TABLE `cad_contato` (
   `celular` text NOT NULL,
   `email` text NOT NULL,
   `skype` text NOT NULL,
-  `observacao` text NOT NULL
+  `site` text NOT NULL,
+  `observacao` text NOT NULL,
+  `usuario` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `cad_contato`
 --
 
-INSERT INTO `cad_contato` (`id_contato`, `nome`, `data_nasc`, `endereco`, `bairro`, `cep`, `cidade`, `estado`, `pais`, `telefone`, `fax`, `celular`, `email`, `skype`, `observacao`) VALUES
-(1, 'Ronaldo Pedro Chaves Soares', '1998-02-03', 'Rua L 1552W', 'Jardim Buritis', '78300000', 'Tangara da Serra', 'Mato Grosso', 'Brasil', '6533251111', '', '65996407733', 'ti@viafertil.com.br', 'ti.viafertil', 'Rapaz que trabalha na TI'),
-(2, 'Max Marques Silveira', '1991-01-01', 'Rua 26 547N', 'Centro', '78300000', 'Tangara da Serra', 'Mato Grosso', 'Brasil', '6533264845', '', '65996845641', 'diretoradm@viafertil.com.br', 'diretoradm', 'Diretor adm Max');
+INSERT INTO `cad_contato` (`id_contato`, `nome`, `data_nasc`, `endereco`, `bairro`, `cep`, `cidade`, `estado`, `pais`, `telefone`, `fax`, `celular`, `email`, `skype`, `site`, `observacao`, `usuario`) VALUES
+(19, 'asfasfasfas', '16/02/2017', 'asdfasfas', 'asfasdf', 'asfasfas', 'asfsadfas', 'Amapá', 'asdfasdfa', 'asdfasf', 'asdfsadfas', 'dasfasdfas', 'dfsadfsafas', 'dfasdfas', 'dfasfas', 'fsafsadfasfas', 'ronaldo'),
+(20, 'asdfewrwer wwcgfdfgcvbcvb', '17/02/2017', 'cvbxcbxc', 'xcvbxcbcx', 'vbcxbxc', 'bcxbcx', 'Amapá', 'bxcbxcbcx', 'bcxvbcx', 'bcxv', 'bcxvbxcvb', 'xcbcxvbcx', 'bxcbxcb', 'cxvbxcb', 'xcbvcxvbxcvbcxv', 'ronaldo'),
+(21, 'wexcvxcvxcvxcvxc', '23/02/2017', 'xcvxcvx', 'cvxcv', 'xcvxcv', 'xcvxcvx', 'Amazonas', 'xcvxcvxc', 'vxcvx', 'cvxcv', 'xcvxcvxcvxc', 'vxcvxcvxc', 'vxcvxc', 'vxcvxcvxcvxcvxc', 'vxcvxcvxcvxc', 'ronaldo'),
+(22, '435325235', '09/02/2017', '23523', '535', '35345325', '235235', 'Alagoas', '235235', '23523', '523', '523523', '523523532532532', '32532', '452353252', '532452345', 'higor');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` text NOT NULL,
+  `login` text NOT NULL,
+  `senha` text NOT NULL,
+  `ativo` int(11) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nome`, `login`, `senha`, `ativo`) VALUES
+(1, 'Ronaldo Pedro', 'ronaldo', '12345', 1),
+(2, 'Higor Luis', 'higor', '12345', 1);
 
 --
 -- Indexes for dumped tables
@@ -64,6 +90,12 @@ ALTER TABLE `cad_contato`
   ADD KEY `id_contato` (`id_contato`);
 
 --
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -71,7 +103,12 @@ ALTER TABLE `cad_contato`
 -- AUTO_INCREMENT for table `cad_contato`
 --
 ALTER TABLE `cad_contato`
-  MODIFY `id_contato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_contato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
